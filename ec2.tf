ebf676d87868504b8c6680cd4e09e65ffc7e70b8
gzhhggg <keiichimonoo@gmail.com>
2023-04-22 18:40:12 +0900

    add bastion ec2 with securitygroup

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+# 踏み台サーバー
+resource "aws_instance" "bastion" {
+  ami = "ami-052c9af0c988f8bbd"
+  instance_type = "t2.micro"
+  key_name = "aws_test"
+  subnet_id     = aws_subnet.public["a"].id
+  vpc_security_group_ids = [aws_security_group.bastion.id]
+  tags = {
+    Name = "bastion-server"
+  }
+}
+
+resource "aws_eip" "bastion" {
+  vpc = true
+  instance = aws_instance.bastion.id
+}
