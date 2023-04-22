ff3c0010cde8f48a441d967fa78d0a2a962e2a3d
gzhhggg <keiichimonoo@gmail.com>
2023-04-23 02:11:30 +0900

    add s3

---
+++ s3.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "default" {
+  bucket = var.baketname
+}
+
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket = aws_s3_bucket.default.bucket
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
